8c633a37f1821050ec6634e3b3c78ff09069f8e9
nguyenvanca <nguyenvanca13@gmail.com>
2024-09-26 14:25:14 +0700

    upload

---
+++ src/Sections/PageContent/FeaturesPage/FeaturesPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FeaturesItem from "./FeaturesItem/FeaturesItem";
+
+export default function FeaturesPage() {
+  return (
+    <div className="row gx-lg-5">
+      <FeaturesItem></FeaturesItem>
+      <FeaturesItem></FeaturesItem>
+      <FeaturesItem></FeaturesItem>
+      <FeaturesItem></FeaturesItem>
+      <FeaturesItem></FeaturesItem>
+      <FeaturesItem></FeaturesItem>
+    </div>
+  );
+}
---
+++ src/Sections/PageContent/PageContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FeaturesPage from "./FeaturesPage/FeaturesPage";
+
+export default function PageContent() {
+  return (
+    <section className="pt-4">
+      <div className="container px-lg-5">
+        <FeaturesPage></FeaturesPage>
+      </div>
+    </section>
+  );
+}
